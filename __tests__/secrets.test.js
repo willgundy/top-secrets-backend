119b538b3931eb8f4ef0c7c9abbe6893078641f4
Will Gundy <willgunderson503@gmail.com>
2022-06-22 22:59:34 -0700

    added test for post secrets authorization

--- __tests__/secrets.test.js
+++ __tests__/secrets.test.js
@@ -30,7 +30,6 @@ describe('secret routes', () => {
 
   it('should get secrets list if user is authorized', async () => {
     const [agent] = await registerAndLogin();
-    const loggedInUser = await agent.get('/api/v1/users/me');
 
     const resp = await agent.get('/api/v1/secrets');
     expect(resp.status).toBe(200);
@@ -39,6 +38,21 @@ describe('secret routes', () => {
 
   it('should not allow unauthorized users to get secrets', async () => {
     const resp = await request(app).get('/api/v1/secrets');
+    expect(resp.status).toBe(401);
+    expect(resp.body).toEqual({ status: 401, message: 'You must be signed in to continue' });
+  });
+
+  it('should let authorized users add a secret', async () => {
+    const [agent] = await registerAndLogin();
+
+    const resp = await agent.post('/api/v1/secrets').send({ title: 'lizard people', description: 'you dont know the half of it'});
+    expect(resp.status).toBe(200);
+    const res = await agent.get('/api/v1/secrets');
+    expect(res.body.length).toEqual(3);
+  });
+
+  it('should not let unauthorized users add a secret', async () => {
+    const resp = await request(app).post('/api/v1/secrets').send({ title: 'lizard people', description: 'you dont know the half of it'});
     expect(resp.status).toBe(401);
     expect(resp.body).toEqual({ status: 401, message: 'You must be signed in to continue' });
   });
